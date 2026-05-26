586c3463f755b251697bf94cab4177ee93031f0b
John Michael P. Natingor <michaelnatingor@gmail.com>
2026-05-26 11:27:38 +0800

    Disable refresh token on startup

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,13 +1,5 @@
-import { catchError, of } from 'rxjs';
 import { AccountService } from '@app/_services';
 
 export function appInitializer(accountService: AccountService) {
-    return () => new Promise<void>(resolve => {
-        accountService.refreshToken()
-            .pipe(catchError(() => of(null)))
-            .subscribe({
-                next: () => resolve(),
-                error: () => resolve()
-            });
-    });
+    return () => Promise.resolve();
 }
